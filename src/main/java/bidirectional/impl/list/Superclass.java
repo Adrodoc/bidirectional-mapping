d68a94e4116adc37bbf3dcf53b9d1812058879c5
UffmannA <a.uffmann@sanacorp.de>
2016-11-17 11:11:19 +0100

    Javadoc für runNonRecursive

--- src/main/java/bidirectional/impl/list/Superclass.java
+++ src/main/java/bidirectional/impl/list/Superclass.java
@@ -3,6 +3,11 @@ package bidirectional.impl.list;
 public abstract class Superclass {
   private boolean pendingRelation;
 
+  /**
+   * Führt das angegebene {@link Runnable} aus, falls es sich hierbei nicht um einen rekursiven Aufruf handelt.
+   *
+   * @param runnable die auszuführende Aktion
+   */
   public void runNonRecursive(Runnable runnable) {
     if (pendingRelation)
       return;
